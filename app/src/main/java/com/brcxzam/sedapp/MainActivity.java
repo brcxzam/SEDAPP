a2c8277b2731b377bb64446f59e287dad457a81a
David Baruc Zamorano <deyvith.lol@gmail.com>
2019-11-30 20:28:18 -0600

    Implementación de Apollo-Client y Progres Bar

--- app/src/main/java/com/brcxzam/sedapp/MainActivity.java
+++ app/src/main/java/com/brcxzam/sedapp/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     AnimationDrawable animationDrawable;
+    TextInputLayout email, password;
+    MaterialButton signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +32,62 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        clearError(email);
+        clearError(password);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         animationDrawable.start();
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public static boolean isValidPassword(CharSequence target) {
+        return !TextUtils.isEmpty(target) && target.length() > 8;
+    }
+
+    public void clearError(final TextInputLayout textInputLayout) {
+        Objects.requireNonNull(textInputLayout.getEditText()).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.signIn) {
+            boolean emailV = isValidEmail(Objects.requireNonNull(email.getEditText()).getText());
+            boolean passwordV = isValidPassword(Objects.requireNonNull(password.getEditText()).getText());
+            if (emailV && passwordV) {
+                Snackbar.make(v, "looks good", Snackbar.LENGTH_SHORT)
+                        .show();
+            } else {
+                String message = "Datos Incorrectos";
+                email.setError(message);
+                password.setError(message);
+            }
+        }
     }
 }
