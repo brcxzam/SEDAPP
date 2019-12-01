754ea94f027c016626b3b0098249764ba812f6e8
David Baruc Zamorano <deyvith.lol@gmail.com>
2019-11-30 19:32:35 -0600

    Creación UI Sign In

--- app/src/main/java/com/brcxzam/sedapp/MainActivity.java
+++ app/src/main/java/com/brcxzam/sedapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.brcxzam.sedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView relativeLayout = findViewById(R.id.title);
+
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        animationDrawable.start();
     }
 }
