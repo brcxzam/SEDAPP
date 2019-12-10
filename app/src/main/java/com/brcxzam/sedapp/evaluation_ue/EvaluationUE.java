edfda4fc89cc99715717fd69ff4c342b25875222
David Baruc Zamorano <deyvith.lol@gmail.com>
2019-12-09 19:44:19 -0600

    Scroll and swipe

--- app/src/main/java/com/brcxzam/sedapp/evaluation_ue/EvaluationUE.java
+++ app/src/main/java/com/brcxzam/sedapp/evaluation_ue/EvaluationUE.java
@@ -3,16 +3,20 @@ package com.brcxzam.sedapp.evaluation_ue;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.apollographql.apollo.ApolloCall;
