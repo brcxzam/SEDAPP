96c6034b461cc8271b1fd276a744062f14725160
David Baruc Zamorano <deyvith.lol@gmail.com>
2020-01-02 19:24:10 -0600

    Mapas Config

--- app/src/main/java/com/brcxzam/sedapp/places_ue/PlacesUE.java
+++ app/src/main/java/com/brcxzam/sedapp/places_ue/PlacesUE.java
@@ -3,24 +3,35 @@ package com.brcxzam.sedapp.places_ue;
 
 import android.os.Bundle;
 
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.brcxzam.sedapp.MainActivity;
 import com.brcxzam.sedapp.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlacesUE extends Fragment {
+public class PlacesUE extends Fragment implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private static View view;
+    private final String ERROR_MAP = "MAPA CARGADO " + getClass().getSimpleName();
 
     public PlacesUE() {
         // Required empty public constructor
@@ -30,12 +41,41 @@ public class PlacesUE extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        FloatingActionButton fab = ((MainActivity) getActivity()).findViewById(R.id.fab);
-//        if (fab.isShown()) {
-//            fab.hide();
-//        }
-        return inflater.inflate(R.layout.fragment_places_ue, container, false);
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_places_ue, container, false);
+        } catch (InflateException e) {
+            Log.e(ERROR_MAP, e.getMessage());
+        }
+
+        if (getActivity() == null) return view;
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.places);
+
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            FragmentManager manager = getChildFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            transaction.replace(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+        return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
